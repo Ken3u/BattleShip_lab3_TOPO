b4dfc210564185384c5940f88a5f7aaf127aba4d
Your Name <you@example.com>
2023-05-02 23:41:23 +0700

    Refactoring generate_enemy_ships() trivial implementation for pass the test

--- logic.py
+++ logic.py
@@ -1,9 +1,11 @@
 import random
 
 # функция генерации кораблей
-def generate_enemy_ships():
-    # Позже реализовать метод генерации кораблей
-    return 0
+def generate_enemy_ships(size):
+    if size == 0:
+        return 0
+    enemy_ships = [[0 for i in range(size)] for i in range(size)]
+    return enemy_ships
 
 
 
--- testGame.py
+++ testGame.py
@@ -8,11 +8,11 @@ class TestGame(unittest.TestCase):
     #     self.assertTrue('logic' in globals())
 
     def test_generate_enemy_ships(self):
-        self.assertEqual(generate_enemy_ships(), 0)
+        self.assertEqual(generate_enemy_ships(0), 0)
 
     def test_generate_enemy_ships_size_5(self):
         enemy_ships = [[0 for i in range(5)] for i in range(5)]
-        self.assertEqual(generate_enemy_ships(), enemy_ships)
+        self.assertEqual(generate_enemy_ships(5), enemy_ships)
 
 
 
